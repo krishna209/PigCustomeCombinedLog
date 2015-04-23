114ee08393518e41305284c32631edace0542158
krishna209 <krishna.kadigari@gmail.com>
2015-04-23 12:39:40 -0700

    Update CustomCombinedLog.java

--- src/main/java/PigCombinedLog/PigApacheLog/CustomCombinedLog.java
+++ src/main/java/PigCombinedLog/PigApacheLog/CustomCombinedLog.java
@@ -4,7 +4,7 @@ import java.util.regex.Pattern;
 
 public class CustomCombinedLog extends RegExLoader{
 	
-	private final static Pattern customCombinedLogPattern = Pattern.compile("^(\\S+)\\s+(\\S+)\\s+(\\S+?)\\s+.([A-Z,a-z,0-9,:,-]*).\\s+.([A-Z]*)\\s+([A-Z,/,a-z,.,\\s+,0-9]*).\\s+([0-9]*)\\s+([0-9]*)\\s+.([-]).\\s+.([A-Z,a-z,/,0-9,.]*)\\s+.([A-Z,0-9,a-z,;,\\s+,_,:,.,(,),/]*).");
+	private final static Pattern customCombinedLogPattern = Pattern.compile("^(\\S+)\\s+(\\S+)\\s+(\\S+)\\s+.([0-9,A-Z,:,-]*).\\s+.([A-Z]*)\\s+(\\S+)\\s+(\\S+)\\s+([0-9]*)\\s+([0-9]*)\\s+.([-]).\\s+.([A-Z,a-z,0-9,/,.,\\s+,(,),;,_,:]*).");
 	public Pattern getPattern()
 	{
 		return customCombinedLogPattern;
